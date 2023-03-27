aefd10cf9ba235d0daa570f4a4078364065c1223
Suhas Maddali <suhas.maddali.chinnu@gmail.com>
2023-03-27 14:58:52 -0400

    Added basic home.html file to upload images

--- CNN Flask API.py
+++ CNN Flask API.py
@@ -1,14 +1,19 @@
 import os
 import flask
 import numpy as np
-import cv2
+from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import load_model
 
 model = load_model("Models/Mobilenet.h5")
 app = flask.Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def home():
+@app.route("/")
+def index():
+    # Render the home page template with the image upload form
+    return flask.render_template("home.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
     if flask.request.method == "POST":
         # Get the uploaded file from the request
         file = flask.request.files["file"]
@@ -18,25 +23,21 @@ def home():
         file.save(file_path)
 
         # Load the image file and preprocess it
-        img = cv2.imread(file_path, target_size=(224, 224))
+        img = image.load_img(file_path, target_size = (224, 224))
         x = image.img_to_array(img)
-        x = np.expand_dims(x, axis=0)
-        x = preprocess_input(x)
+        x = np.expand_dims(x, axis = 0)
 
         # Make predictions using the loaded model
         predictions = model.predict(x)
 
         # Get the predicted class label
-        label = np.argmax(predictions[0])
+        if predictions[0][0] > 0.25:
+            label = "Dusty"
+        else:
+            label = "Clean"
 
-        # Return the predicted class label as a response
-        return flask.render_template("result.html", label=label)
+        # Render the result template with the predicted class label
+        return flask.render_template("result.html", label = label)
 
-    else:
-        # Render the home page template with the image upload form
-        return flask.render_template("home.html")
-
-
-
-if __name__ == '__main__':
-    app.run(debug = True)
+if __name__ == "__main__":
+    app.run(debug=True)
