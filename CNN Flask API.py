686945c89c14883ced8d1d724801fe943f207e64
Suhas Maddali <suhas.maddali.chinnu@gmail.com>
2023-03-27 14:26:39 -0400

    Added generated images

--- CNN Flask API.py
+++ CNN Flask API.py
@@ -1,23 +1,42 @@
+import os
 import flask
+import numpy as np
+import cv2
 from tensorflow.keras.models import load_model
 
 model = load_model("Models/Mobilenet.h5")
 app = flask.Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return flask.render_template("home.html")
+    if flask.request.method == "POST":
+        # Get the uploaded file from the request
+        file = flask.request.files["file"]
 
-@app.route("/predict", methods=["POST"])
-def predict():
-    # Get the input data from the request
-    input_data = flask.request.json["input_data"]
+        # Save the file to a temporary directory
+        file_path = os.path.join(app.root_path, "temp", file.filename)
+        file.save(file_path)
 
-    # Make predictions using the loaded model
-    predictions = model.predict(input_data)
+        # Load the image file and preprocess it
+        img = cv2.imread(file_path, target_size=(224, 224))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
 
-    # Return the predictions as a JSON response
-    return flask.jsonify(predictions.tolist())
+        # Make predictions using the loaded model
+        predictions = model.predict(x)
+
+        # Get the predicted class label
+        label = np.argmax(predictions[0])
+
+        # Return the predicted class label as a response
+        return flask.render_template("result.html", label=label)
+
+    else:
+        # Render the home page template with the image upload form
+        return flask.render_template("home.html")
+
+
 
 if __name__ == '__main__':
     app.run(debug = True)
